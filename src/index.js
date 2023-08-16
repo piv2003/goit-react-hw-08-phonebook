7d29f43ffdee069cf9fdb6304201d87c02cee0d8
Ihor Poliakov <piv2003@gmail.com>
2023-08-16 20:10:40 +0200

    globalStyles&index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
+import GlobalStyle from './GlobalStyles';
+import { App } from './components/App';
+import { store, persistor } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
