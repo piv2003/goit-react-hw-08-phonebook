ea572705440379a1865fa61f041b815d48714b68
Ihor Poliakov <piv2003@gmail.com>
2023-08-16 21:24:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,13 @@ import { store, persistor } from './redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter basename="/goit-react-hw-08-movies">
+          <App />
+          <GlobalStyle />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
