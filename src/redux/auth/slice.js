9976b2ad4950ef417812820b01d574d5e8337597
Ihor Poliakov <piv2003@gmail.com>
2023-08-18 00:20:11 +0200

    Update slice.js

---
+++ src/redux/auth/slice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { logIn, logOut, refreshUser, register } from './operations';
+
+const initialState = {
+  user: {
+    name: null,
+    email: null,
+  },
+  token: null,
+  isLoggedIn: false,
+  isRefreshing: false,
+  isLoading: false,
+};
