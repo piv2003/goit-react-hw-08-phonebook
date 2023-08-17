1cf5383f1aaff88f56169baa5f135845440fa96a
Ihor Poliakov <piv2003@gmail.com>
2023-08-18 00:06:44 +0200

    Update Home.jsx

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import { Helmet } from 'react-helmet';
+import { Link, NavLink } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
+import { TitleWrapper, Container } from './Home.styled';
+
+const Home = () => {
+    return ();
+};
+
+export default Home;
