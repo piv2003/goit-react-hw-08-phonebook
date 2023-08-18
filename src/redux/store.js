c375f2312854e862f0490f2ee2dca44087184d96
Ihor Poliakov <piv2003@gmail.com>
2023-08-18 17:21:06 +0200

    Update store.js

---
+++ src/redux/store.js
@@ -0,0 +1,17 @@
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { authReducer } from './auth/slice';
+import { contactsReducer } from './contacts/slice';
+import { filterReducer } from './filrer/slice';
+
+export const persistor = persistStore(store);
