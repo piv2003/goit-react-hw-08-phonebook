5b7dba15d4353eda30e41a7d463ad80eedafa6f2
Ihor Poliakov <piv2003@gmail.com>
2023-08-18 17:39:55 +0200

    Update Navigation.jsx

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,13 @@
+import { AiFillHome } from 'react-icons/ai';
+import { GiBookmarklet } from 'react-icons/gi';
+import { useAuth } from '../../hooks/useAuth';
+import { Nav, Link } from './Navigation.styled';
+
+const Navigation = () => {
+  const { isLoggedIn } = useAuth();
+
+  return (    
+  );
+};
+
+export default Navigation;
