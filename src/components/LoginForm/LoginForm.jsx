294f91b256c432bcf5ae67e6e0288e5ab33b4758
Ihor Poliakov <piv2003@gmail.com>
2023-08-18 17:34:21 +0200

    Update LoginForm.jsx

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,62 +1,32 @@
-import PropTypes from 'prop-types';
-import { Component } from 'react';
-import { FormBox, InputName, InputTel, Button } from './LoginForm.styled';
-class Form extends Component {
-  state = {
-    name: '',
-    number: '',
+import { useDispatch } from 'react-redux';
+import { ErrorMessage, Formik } from 'formik';
+import { object, string } from 'yup';
+import { FiMail } from 'react-icons/fi';
+import { RiLockPasswordFill } from 'react-icons/ri';
+import { logIn } from '../../redux/auth/operations';
+import {
+  ErrorMessageText,
+  Container,
+  Label,
+  Text,
+  Input,
+  FormBox,
+  SubmitButton,
+  Title,
+} from './LoginForm.styled';
+
+
+const LoginForm = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = ({ email, password }, { resetForm }) => {
+    dispatch(logIn({ email, password }));
+    resetForm();
   };
 
-  handleChange = e => {
-    const { name, value } = e.currentTarget;
-    this.setState({
-      [name]: value,
-    });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.setState({ name: '', number: '' });
-  };
-
-  render() {
-    const { name, number } = this.state;
-    return (
-      <FormBox onSubmit={this.handleSubmit} autoComplete="off">
-        <label>
-          <span>Name</span>
-          <InputName
-            type="text"
-            name="name"
-            value={name}
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. 
-        For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            onChange={this.handleChange}
-          />
-        </label>
-        <label>
-          <span>Number</span>
-          <InputTel
-            type="tel"
-            name="number"
-            value={number}
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            onChange={this.handleChange}
-          />
-        </label>
-        <Button type="submit">Add contact</Button>
-      </FormBox>
-    );
-  }
-}
-
-Form.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
+  return (
+    
+  );
 };
 
-export default Form;
+export default LoginForm;
