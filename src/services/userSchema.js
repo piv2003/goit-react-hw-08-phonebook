3904494a2e1d82b140a5964edebe4846c360416f
Ihor Poliakov <piv2003@gmail.com>
2023-08-19 22:18:33 +0200

    Create userSchema.js

---
+++ src/services/userSchema.js
@@ -0,0 +1,16 @@
+import { object, string } from 'yup';
+
+export const userSchema = object({
+  name: string()
+    .matches(
+      /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/,
+      "Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan",
+    )
+    .required(),
+  number: string()
+    .matches(
+      /\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}/,
+      'Phone number must be digits and can contain spaces, dashes, parentheses and can start with +',
+    )
+    .required(),
+});
