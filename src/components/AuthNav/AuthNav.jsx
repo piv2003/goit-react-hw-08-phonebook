276bf37d5fe105ee3a92de42e4d350f2e981e6ac
Ihor Poliakov <piv2003@gmail.com>
2023-08-21 00:45:00 +0200

    Update AuthNav.jsx

---
+++ src/components/AuthNav/AuthNav.jsx
@@ -0,0 +1,7 @@
+import { FaRegRegistered } from 'react-icons/fa';
+import { BiLogInCircle } from 'react-icons/bi';
+import { AuthLink, Wrapper } from './AuthNav.styled';
+
+const AuthNav = () => {};
+
+export default AuthNav;
