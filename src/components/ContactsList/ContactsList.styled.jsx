d7ebcef17904f1e69104351116d47f636b9362d5
Ihor Poliakov <piv2003@gmail.com>
2023-08-21 00:58:31 +0200

    Update ContactsList.styled.jsx

--- src/components/ContactsList/ContactsList.styled.jsx
+++ src/components/ContactsList/ContactsList.styled.jsx
@@ -1,8 +1,36 @@
 import styled from 'styled-components';
 
+export const Wrapper = styled.div`
+  gap: 20px;
+
+  & b {
+    font-size: 20px;
+    color: red;
+    text-shadow: 2px 2px #000;
+  }
+`;
+
 export const List = styled.ul`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
-  grid-column-gap: 10px;
-  grid-row-gap: 10px;
+  grid-template-columns: repeat(3, 45vh);
+  grid-template-rows: repeat(3, 70px);
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 10px;
+`;
+
+export const Item = styled.li`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  justify-content: space-between;
+  padding: 10px;
+  border-radius: 50px;
+  color: #fff;
+  background: rgba(238, 95, 202, 0.3);
+  box-shadow: rgba(0, 0, 0, 0.17) 0px -23px 25px 0px inset,
+    rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset,
+    rgba(0, 0, 0, 0.1) 0px -79px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px,
+    rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px,
+    rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
 `;
