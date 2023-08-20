094a40190e9a3584782d11863c2abf54800a0f67
Ihor Poliakov <piv2003@gmail.com>
2023-08-21 00:45:43 +0200

    Update AuthNav.jsx

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -2,6 +2,17 @@ import { FaRegRegistered } from 'react-icons/fa';
 import { BiLogInCircle } from 'react-icons/bi';
 import { AuthLink, Wrapper } from './AuthNav.styled';
 
-const AuthNav = () => {};
+const AuthNav = () => {
+  return (
+    <Wrapper>
+      <AuthLink to="/register">
+        <FaRegRegistered /> Register
+      </AuthLink>
+      <AuthLink to="/login">
+        <BiLogInCircle /> Log in
+      </AuthLink>
+    </Wrapper>
+  );
+};
 
 export default AuthNav;
