d0424884b32b7d47955801d16c896d86ab3d02c3
Ihor Poliakov <piv2003@gmail.com>
2023-08-21 00:59:32 +0200

    Update Filter.styled.jsx

--- src/components/Filter/Filter.styled.jsx
+++ src/components/Filter/Filter.styled.jsx
@@ -7,23 +7,40 @@ export const LabelFilter = styled.label`
 
   & span {
     margin-right: 10px;
-    font-size: 18px;
-    font-weight: 700;
-    color: #d4f783;
-    text-shadow: 3px 5px 2px #474747;
+    padding: 5px;
+    font-size: 20px;
+    color: #f1bc49;
+    background: rgba(3, 60, 146, 0.3);
+    border-radius: 15px;
+    text-shadow: 2px 3px 2px #474747;
   }
 `;
 
 export const InputFilter = styled.input`
   padding: 0 20px;
-  background: #f6f6e9;
+  background: rgba(255, 250, 250, 0.4);
   line-height: 40px;
   border-width: 0;
   border-radius: 20px;
   font-family: 'Roboto', sans-serif;
-  font-style: normal;
   font-size: 18px;
-  box-shadow: rgba(0, 0, 0, 0.45) 0px 75px 75px,
-    rgba(0, 0, 0, 0.32) 0px -12px 30px, rgba(0, 0, 0, 0.12) 0px 4px 6px,
-    rgba(0, 0, 0, 0.37) 0px 12px 21px, rgba(0, 0, 0, 0.09) 0px -3px 2px;
+  box-shadow: rgba(0, 0, 0, 0.2) 0px 2px 4px;
+
+  &::-webkit-input-placeholder {
+    /* WebKit browsers */
+    color: rgba(255, 255, 0, 0.5);
+  }
+  :-moz-placeholder {
+    /* Mozilla Firefox 4 to 18 */
+    color: rgba(255, 255, 0, 0.5);
+  }
+  ::-moz-placeholder {
+    /* Mozilla Firefox 19+ */
+    color: rgba(255, 255, 0, 0.5);
+  }
+
+  &:focus {
+    outline: none;
+    border: solid 2px #560bad;
+  }
 `;
