3c6ed53f6a5b25ebd80a724686d92709c0bebc80
Ihor Poliakov <piv2003@gmail.com>
2023-08-21 11:44:08 +0200

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,45 @@
 import { Helmet } from 'react-helmet';
 import { Link, NavLink } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
-import { TitleWrapper, Container } from './Home.styled';
+import {
+  TitleWrapper,
+  Container,
+  ButtonRegistr,
+  ButtonLogIn,
+} from './Home.styled';
 
 const Home = () => {
-    return ();
+  const { isLoggedIn } = useAuth();
+
+  return (
+    <>
+      <Helmet>
+        <title>Phonebook</title>
+      </Helmet>
+
+      {!isLoggedIn ? (
+        <TitleWrapper>
+          <Container>
+            <span>Welcome, please</span>
+            <NavLink to="/register">
+              <ButtonRegistr>Register</ButtonRegistr>
+            </NavLink>
+            <span>or</span>
+            <NavLink to="/login">
+              <ButtonLogIn>Log in</ButtonLogIn>
+            </NavLink>{' '}
+            <span>to use the Phonebook</span>
+          </Container>
+        </TitleWrapper>
+      ) : (
+        <TitleWrapper>
+          <Container>
+            Please go to {<Link to="/contacts">Contacts</Link>} to use the app
+          </Container>
+        </TitleWrapper>
+      )}
+    </>
+  );
 };
 
 export default Home;
