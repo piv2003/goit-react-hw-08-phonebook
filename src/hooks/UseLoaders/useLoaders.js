a17ec2f8a1b308b6ea1316efb2441a5622274f76
Ihor Poliakov <piv2003@gmail.com>
2023-08-21 11:40:52 +0200

    Update useLoaders.js

--- src/hooks/UseLoaders/useLoaders.js
+++ src/hooks/UseLoaders/useLoaders.js
@@ -1,4 +1,23 @@
 import { Puff } from 'react-loader-spinner';
-import styled from 'styled-components';
+import { LoaderBox } from './useLoaders.styled';
 
-export const useLoaders = () => {};
+export const useLoaders = () => {
+  const LoaderBig = () => {
+    return (
+      <LoaderBox>
+        <Puff
+          height="150"
+          width="150"
+          radius={1}
+          color="#560bad"
+          ariaLabel="puff-loading"
+          wrapperStyle={{}}
+          wrapperClass=""
+          visible={true}
+        />
+      </LoaderBox>
+    );
+  };
+
+  return { LoaderBig };
+};
