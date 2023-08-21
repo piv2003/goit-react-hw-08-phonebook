94b8939bb38a60b4fc9527050b4b3d6b80512279
Ihor Poliakov <piv2003@gmail.com>
2023-08-21 11:53:11 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -14,4 +14,28 @@ import { authReducer } from './auth/slice';
 import { contactsReducer } from './contacts/slice';
 import { filterReducer } from './filrer/slice';
 
+const middleware = [
+  ...getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+];
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+export const store = configureStore({
+  reducer: {
+    auth: persistReducer(authPersistConfig, authReducer),
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
+  middleware,
+  devTools: process.env.NODE_ENV === 'development',
+});
+
 export const persistor = persistStore(store);
