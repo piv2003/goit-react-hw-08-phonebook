47a03cbb63a2e40249a959178632d08855b3701a
Ihor Poliakov <piv2003@gmail.com>
2023-08-21 11:41:22 +0200

    Update useLoaders.styled.jsx

---
+++ src/hooks/UseLoaders/useLoaders.styled.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const LoaderBox = styled.div`
+  position: absolute;
+  z-index: 10;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 113%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.5);
+`;
