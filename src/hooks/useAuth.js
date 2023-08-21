26102d7252f00ebda237c96111d29015ab938c63
Ihor Poliakov <piv2003@gmail.com>
2023-08-21 11:42:12 +0200

    Update useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -6,4 +6,16 @@ import {
   selectIsLoading,
 } from '../redux/auth/selectors';
 
-export const useAuth = () => {};
+export const useAuth = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isRefreshing = useSelector(selectIsRefreshing);
+  const user = useSelector(selectUser);
+  const isLoading = useSelector(selectIsLoading);
+
+  return {
+    isLoggedIn,
+    isRefreshing,
+    user,
+    isLoading,
+  };
+};
