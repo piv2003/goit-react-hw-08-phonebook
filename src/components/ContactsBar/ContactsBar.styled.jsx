3b0becb1a74542cdfc803cdfca2a5d00fb6cfa7b
Ihor Poliakov <piv2003@gmail.com>
2023-08-23 23:29:11 +0200

    Update ContactsBar.styled.jsx

--- src/components/ContactsBar/ContactsBar.styled.jsx
+++ src/components/ContactsBar/ContactsBar.styled.jsx
@@ -23,9 +23,10 @@ export const Wrapper = styled.div`
     22%,
     25%,
     53%,
-    57% {
-      text-shadow: 0 0 4px #fff, 0 0 11px #fff, 0 0 19px #fff, 0 0 40px #0fa,
-        0 0 80px #0fa, 0 0 90px #0fa, 0 0 100px #0fa, 0 0 150px #0fa;
+    87% {
+      text-shadow: 0 0 4px #8a058a, 0 0 11px #8a058a, 0 0 19px #8a058a,
+        0 0 40px #0fa, 0 0 80px #0fa, 0 0 90px #0fa, 0 0 100px #0fa,
+        0 0 150px #0fa;
     }
     10%,
     20%,
